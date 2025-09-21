c370d3874efc7b6c5325322caa84b31d52a2b734
G4rb1n <106922496+G4rb1n@users.noreply.github.com>
2025-09-21 16:20:38 -0300

    Update CorsConfig.java

--- zadinventory/src/main/java/app/zad/zadinventory/config/CorsConfig.java
+++ zadinventory/src/main/java/app/zad/zadinventory/config/CorsConfig.java
@@ -13,10 +13,9 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/*")
-                        .allowedOrigins("http://localhost:4200")
+                registry.addMapping("/**")        // Qualquer endpoint
+                        .allowedOrigins("http://localhost:4200") // Seu front-end
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("")
                         .allowCredentials(true);
             }
         };
